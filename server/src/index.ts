ff01e706a3b2c368091428873ee85106212c0078
shashankjt <shashankjt11@gmail.com>
2026-01-16 15:29:54 +0530

    fixed home route

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,6 +16,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Basic route to check server status
+app.get('/', (req, res) => {
+    res.send('RentRoost API is working!');
+});
+
 // Serve static files from the "House Images" directory
 import path from 'path';
 app.use('/images', express.static(path.join(__dirname, '../data/House Images')));
